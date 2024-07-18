fc4477f03aa66d0b730198e185fb8a225a0bdfca
Egor Melnikov <pc.player@mail.ru>
2024-07-18 23:09:15 +0300

    First_commit

---
+++ Caesar.py
@@ -0,0 +1,23 @@
+from tkinter import *
+import tkinter as tk
+
+# Window
+
+root=Tk()
+root.title("Caesar Ipher")
+root.geometry('1280x720')
+root.resizable(False, False)
+root.configure(bg='white')
+
+# Buttons
+
+btn1 = tk.Button(root, text='Encryption', height=10, width=70)
+btn1.grid(row=1, column=0, pady=5)
+btn1.place(relx=0.5, rely=0.5, anchor=CENTER, y=-100)
+
+btn2 = tk.Button(root, text='Decryption', height=10, width=70)
+btn2.grid(row=2, column=0, pady=5)
+btn2.place(relx=0.5, rely=0.5, anchor=CENTER, y=100)
+
+# On/Off
+root.mainloop()
