08dec88a6755ad162e87c1dabfbc9ec5327d7ed7
Egor Melnikov <pc.player@mail.ru>
2024-07-27 19:31:40 +0300

    Buttons_fix

--- Caesar.py
+++ Caesar.py
@@ -14,20 +14,94 @@ root.configure(bg='white')
 # ENG/RUS
 
 def ENG():
-    English = Tk()
+    global English
+
+    English = Toplevel()
     English.title("Task")
     English.geometry('1280x720')
     English.resizable(False, False)
     English.configure(bg='white')
-    root.destroy()
+
+    # Buttons
+
+    btn_Encr_ENG = ttk.Button(English, text='Encryption', height=10, width=70, cursor="hand2", command=Encr_ENG)
+    btn_Encr_ENG.grid(row=1, column=0, pady=5)
+    btn_Encr_ENG.place(relx=0.5, rely=0.5, anchor=CENTER, y=-100)
+
+    btn_Decr_ENG = ttk.Button(English, text='Decryption', height=10, width=70, cursor="hand2", command=Decr_ENG)
+    btn_Decr_ENG.grid(row=2, column=0, pady=5)
+    btn_Decr_ENG.place(relx=0.5, rely=0.5, anchor=CENTER, y=100)
+
+    root.withdraw()
 
 def RUS():
-    Russian = Tk()
+    global Russian
+
+    Russian = Toplevel()
     Russian.title("Задача")
     Russian.geometry('1280x720')
     Russian.resizable(False, False)
     Russian.configure(bg='white')
-    root.destroy()
+
+    # Buttons
+
+    btn_Encr_RUS = ttk.Button(Russian, text='Шифрование', height=10, width=70, cursor="hand2", command=Encr_RUS)
+    btn_Encr_RUS.grid(row=1, column=0, pady=5)
+    btn_Encr_RUS.place(relx=0.5, rely=0.5, anchor=CENTER, y=-100)
+
+    btn_Decr_RUS = ttk.Button(Russian, text='Дешифрование', height=10, width=70, cursor="hand2", command=Decr_RUS)
+    btn_Decr_RUS.grid(row=2, column=0, pady=5)
+    btn_Decr_RUS.place(relx=0.5, rely=0.5, anchor=CENTER, y=100)
+
+    root.withdraw()
+
+# ENG -> Encryption & Decryption
+
+def Encr_ENG():
+    global Encryption_ENG
+
+    Encryption_ENG = Toplevel()
+    Encryption_ENG.title("Encryption")
+    Encryption_ENG.geometry('1280x720')
+    Encryption_ENG.resizable(False, False)
+    Encryption_ENG.configure(bg='white')
+
+    English.withdraw()
+
+def Decr_ENG():
+    global Decryption_ENG
+
+    Decryption_ENG = Toplevel()
+    Decryption_ENG.title("Decryption")
+    Decryption_ENG.geometry('1280x720')
+    Decryption_ENG.resizable(False, False)
+    Decryption_ENG.configure(bg='white')
+
+    English.withdraw()
+
+# RUS -> Encryption & Decryption
+
+def Encr_RUS():
+    global Encryption_RUS
+
+    Encryption_RUS = Toplevel()
+    Encryption_RUS.title("Шифрование")
+    Encryption_RUS.geometry('1280x720')
+    Encryption_RUS.resizable(False, False)
+    Encryption_RUS.configure(bg='white')
+
+    Russian.destroy()
+
+def Decr_RUS():
+    global Decryption_RUS
+
+    Decryption_RUS = Toplevel()
+    Decryption_RUS.title("Дешифрование")
+    Decryption_RUS.geometry('1280x720')
+    Decryption_RUS.resizable(False, False)
+    Decryption_RUS.configure(bg='white')
+
+    Russian.destroy()
 
 # Buttons ENG/RUS
 
@@ -38,34 +112,6 @@ btn_ENG.place(relx=0.5, rely=0.5, anchor=CENTER, y=-100)
 btn_RUS = ttk.Button(root, text='RUSSIAN', height=10, width=70, cursor="hand2", command=RUS)
 btn_RUS.grid(row=2, column=0, pady=5)
 btn_RUS.place(relx=0.5, rely=0.5, anchor=CENTER, y=100)
-
-# Encryption & Decryption
-
-def click1():
-    Encryption = Tk()
-    Encryption.title("Encryption")
-    Encryption.geometry('1280x720')
-    Encryption.resizable(False, False)
-    Encryption.configure(bg='white')
-    root.destroy()
-
-def click2():
-    Decryption = Tk()
-    Decryption.title("Decryption")
-    Decryption.geometry('1280x720')
-    Decryption.resizable(False, False)
-    Decryption.configure(bg='white')
-    root.destroy()
-
-# Buttons
-
-btn1 = ttk.Button(root, text='Encryption', height=10, width=70, cursor="hand2", command=click1)
-btn1.grid(row=1, column=0, pady=5)
-btn1.place(relx=0.5, rely=0.5, anchor=CENTER, y=-100)
-
-btn2 = ttk.Button(root, text='Decryption', height=10, width=70, cursor="hand2", command=click2)
-btn2.grid(row=2, column=0, pady=5)
-btn2.place(relx=0.5, rely=0.5, anchor=CENTER, y=100)
 
 # On/Off
 
